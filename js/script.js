371fb95932310ddc14870b70c82248fffcbd56db
Martin PCescritorio <bergesio90@gmail.com>
2022-06-02 19:39:57 +0200

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 const $AdviceNumber = document.querySelector(".mainCard__adviceNumber"),
   $AdviceText = document.querySelector(".mainCard__adviceText"),
-  $AdviceButton = document.querySelector(".mainCard__button");
+  $AdviceButton = document.querySelector(".mainCard__advice__button");
 
 function getAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
       $AdviceNumber.innerHTML = `ADVICE #${data.slip.id}`;
-      $AdviceText.innerHTML = data.slip.advice;
+      $AdviceText.innerHTML = `❝ ${data.slip.advice} ❞`;
     });
 }
 
-/* getAdvice(); */
-
 window.addEventListener("click", (e) => {
-  console.log(e.target);
   if (e.target === $AdviceButton) {
     getAdvice();
   }
